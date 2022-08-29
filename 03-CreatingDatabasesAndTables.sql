6529a3913d528e98f6c9834d1b0a3273ad910bae
E. S. Adsiz <98318801+esadsiz@users.noreply.github.com>
2022-08-29 22:27:11 +0300

    Update 03-CreatingDatabasesAndTables.sql

--- 03-CreatingDatabasesAndTables.sql
+++ 03-CreatingDatabasesAndTables.sql
@@ -3,14 +3,17 @@
 CREATE TABLE account
 -- tablo ismi 'account'
 (user_id SERIAL PRIMARY KEY,
+-- bu sayede tabloya eklenen her yeni satirda bu id 3,4,5,6 seklinde artarak gider.
+-- bir tabloda sadece ve sadece bir adet PRIMARY KEY olabilir
 username VARCHAR(50) UNIQUE not NULL,
--- VARCHAR(50) sahip olacagi deger en fazla 50 karakter uzunlugunda olabilir,
+-- VARCHAR metinsel alan demek, 50 ise sahip olacagi maximum karakter sayisi demektir.
+-- Bir de CHAR(10) vardir, bu da degerin örnegin tam olarak 10 karakterden olusmasi gerektigini belirtir. 
 -- UNIQUE ayni degere sadece bir kez sahip olabilir.
 -- NOT NULL bos birakilamaz.
 PASSWORD VARCHAR(50) not null,
 email VARCHAR(50) UNIQUE not NULL,
 created_on TIMESTAMP not NULL,
--- TIMESTAMP zaman damgasi basar.
+-- TIMESTAMP zaman damgasi tipidir.
 last_login TIMESTAMP
 );
 
