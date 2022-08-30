8d4bb9c0f55a4c2a2310ee33fddd598a02229fcf
E. S. Adsiz <98318801+esadsiz@users.noreply.github.com>
2022-08-30 19:07:14 +0300

    Update 03-CreatingDatabasesAndTables.sql

--- 03-CreatingDatabasesAndTables.sql
+++ 03-CreatingDatabasesAndTables.sql
@@ -35,7 +35,15 @@ CREATE TABLE account_job(
 -- INSERT
 
 INSERT INTO account(username, password, email, created_on)
+-- INSERT tabloya yeni bir kayit eklemek icin kullanilir.
+-- Hangi tabloya? account tablosuna.
+-- Parantez icine de hangi alanlara kayit eklemek istiyorsak onlari yaziyoruz.
 VALUES ('Enes', 'enes123', 'enes@mail.com', current_timestamp)
+-- kayitlara eklenecek degerler de sirasiyla bu sekilde yazilir.
+
+INSERT INTO CustomersWork (CustomerId, CompanyName, ContactName) SELECT CustomerId, CompanyName, ContactName from Customers;
+-- Customers tablosunun yazili sütunlarini, CustomersWork tablosunun yazili sütunlarina yerlestir.
+-- Komut satirinin sonuna bir de WHERE ContactName LIKE '%en%'; eklersek, sütunun tamamini degil, yalnizca sarta uyan satirlari alip yerlestirir.
 
 INSERT INTO account_job(user_id, job_id, hire_date)
 VALUES (1, 1, current_timestamp)
@@ -47,6 +55,14 @@ VALUES (1, 1, current_timestamp)
 -- UPDATE
 
 UPDATE account SET last_login = CURRENT_TIMESTAMP;
+-- UPDATE veritabaninda sarta uyan kayitlari güncellemek icin kullanilir.
+-- account tablosunun last_login sütununu CURRENT_TIMESTAMP olarak ayarla.
+-- burada tüm sütun degisir. daha spesifik olmak icin WHERE komutu ile filtreleme yapabiliriz.
+
+UPDATE Customers SET CompanyName = CustomersWork.CompanyName FROM Customers INNER JOIN CustomersWork USING(CustomerID);
+-- Customers tablosunun CompanyName sütununu (komple) degistir. Ne ile? CustomersWork tablosunun CompanyName sütunu ile.
+-- Ama bu degisimi Customers tablosu ile CompanyName tablosunun kesisim noktalarini baz alarak yap.
+-- Komut satirinin sonuna bir de WHERE CustomersWork.CompanyName LIKE '%TEST%'; eklersek, sadece bu sarta uyan satirlari alir koyar.
 
 UPDATE account SET last_login = CURRENT_TIMESTAMP RETURNING last_login;
 
@@ -57,6 +73,9 @@ UPDATE account SET last_login = CURRENT_TIMESTAMP RETURNING last_login;
 -- DELETE
 
 DELETE FROM account WHERE username = 'enes';
+-- DELETE veritabaninda sarta uyan kayitlari silmek icin kullanilir.
+
+DELETE Customers FROM Customers INNER JOIN CustomersWork USING(CustomerID) WHERE CustomersWork.CompanyName LIKE '%TEST%';
 
 --
 --
