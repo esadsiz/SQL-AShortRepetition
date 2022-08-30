10b0b6a87b351b194e3e63fe1df8fd0363eb094e
E. S. Adsiz <98318801+esadsiz@users.noreply.github.com>
2022-08-31 02:57:39 +0300

    Update 03-ConditionalExpPro.sql

--- 03-ConditionalExpPro.sql
+++ 03-ConditionalExpPro.sql
@@ -12,8 +12,37 @@ FROM customer;
 -- Nereye? case isimli bir sütuna. customer_id sütununun her bir degeri icin bu sarti kontrol eder ve karsisina
 -- Premium, Plus ya da Normal yazdirir.
 
+SELECT customer_id,
+CASE customer_id
+       WHEN 1 THEN 'Winner'
+       WHEN 2 THEN 'Second Place'
+       ELSE 'Others'
+END
+FROM customer;
+
+-- Eger customer_id 1 ise 'Winner', 2 ise 'Second Place', hicbiri degilse 'Others' yazdir.
+-- Nereye? case isimli bir sütuna. customer_id sütununun her bir degeri icin bu sarti kontrol eder ve karsisina
+-- Winner, Second Place ya da Others yazdirir.
+
+-- CASE'den END'e kadar parantez icine alinip basina senaryoya göre SUM, AVG vs konularak fonksiyona sokulabilir. Bu durumda tabi tek satir tek sütun döndürür.
+
 --
 --
 --
 
+-- COALESCE
+
+SELECT coalesce(firstname, 'no firstname available'), lastname from "public"."customers";
+/* coalesce firsname sütunundaki null degerleri yakalar ve yerlerine null degeri yerine 2. parametrede yazili mesaji bastirir.' */
+
+SELECT SUM(COALESCE(age, 50)) FROM customers;
+/* coalesce age sütunundaki null degerleri yakalar ve degerlerini 50 ile degistirir, ardindan bunlar sum ile toplanir. */
+
 --
+--
+--
+
+-- CAST
+
+SELECT CAST('5' AS INTEGER)
+SELECT '10'::INTEGER
